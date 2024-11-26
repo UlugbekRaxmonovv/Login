e90586381052b5e9b935dce841843d29a7e3a674
Ulug'bek <rahmonovulugbek20042004@gmail.com>
2024-11-26 22:11:13 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Login from "./login";
+import Login from "./Login";
 import Home from "./Home";
 const App = () => {
   return (
